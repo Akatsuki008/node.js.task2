2f6abb9197db4eb7656181068f86ed0e909a8bc1
Pretty Dey <102508697+Akatsuki008@users.noreply.github.com>
2022-04-22 15:37:17 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,2 +1,39 @@
-const prompt = require("prompt-sync")();
-var c = prompt("Enter name ");
+const http = require('http');
+const fs = require('fs');
+const lodash = require('lodash');
+const server = http.createServer((req, res)=>{
+    console.log(req.url, req.method);
+
+    res.setHeader("Content-Type", "text/plain");
+  /*  res.write('<head><link rel="styleseet" href= "#"></head>');
+    res.write("<p>Hello guys</p>");
+    res.write("<p>Hello guys, bye bye</p>");*/
+    //send html file
+    let path = "./views";
+    switch(req.url)
+    {
+        case '/':
+            path+= 'server.html';
+            break;
+        case '/about':
+            path+= 'sabout.html';
+            break;
+        default:
+            path += "404.html";
+            break;
+    }
+fs.readFile('./views/server.html', (err, data) => {
+      if (err) {
+    console.log(err);
+    res.end();
+  } else{
+  res.write(data);
+  res.end(data);
+  }
+})
+
+});
+
+server.listen(3000, 'localhost', ()=>{
+    console.log("listening....")
+});
